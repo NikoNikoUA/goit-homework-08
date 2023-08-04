6ea988d4c24dfa5ee4943c8c14e2f29e9788548e
NikoNikoUA <128256922+NikoNikoUA@users.noreply.github.com>
2023-08-04 16:39:25 +0300

    Task 3 partially done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,61 @@
+import throttle from 'lodash.throttle';
 
+// const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+// const form = document.querySelector('.feedback-form');
+// const textarea = document.querySelector('textarea');
+
+// form.addEventListener('submit', onFormSubmit, 500);
+// textarea.addEventListener('input', throttle(onTextAreaInput, 500));
+
+// onPageLoading();
+
+// function onPageLoading() {
+//   const existingMessage = localStorage.getItem(LOCAL_STORAGE_KEY);
+//   if (existingMessage) {
+//     textarea.value = existingMessage;
+//   }
+// }
+// function onTextAreaInput(event) {
+//   localStorage.setItem(LOCAL_STORAGE_KEY, event.target.value);
+// }
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   event.target.reset();
+//   localStorage.removeItem(LOCAL_STORAGE_KEY);
+// }
+
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+let formData = {};
+
+onPageLoading();
+
+function onPageLoading() {
+  const existingMessage = localStorage.getItem(LOCAL_STORAGE_KEY);
+  if (existingMessage) {
+    const newStaticObject = JSON.parse(existingMessage);
+    console.log(newStaticObject);
+    newStaticObject.name = formData.name;
+    newStaticObject.value = formData.value;
+  }
+}
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.target.reset();
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+  console.log(formData);
+}
