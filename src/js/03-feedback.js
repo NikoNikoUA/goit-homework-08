1c4de59ac8af32b2bcad7cae2df47aff7a4d5da5
NikoNikoUA <128256922+NikoNikoUA@users.noreply.github.com>
2023-08-05 12:51:22 +0300

    Task 3 removed unnecessary parts

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,31 +1,5 @@
 import throttle from 'lodash.throttle';
 
-// const LOCAL_STORAGE_KEY = 'feedback-form-state';
-
-// const form = document.querySelector('.feedback-form');
-// const textarea = document.querySelector('textarea');
-
-// form.addEventListener('submit', onFormSubmit, 500);
-// textarea.addEventListener('input', throttle(onTextAreaInput, 500));
-
-// onPageLoading();
-
-// function onPageLoading() {
-//   const existingMessage = localStorage.getItem(LOCAL_STORAGE_KEY);
-//   if (existingMessage) {
-//     textarea.value = existingMessage;
-//   }
-// }
-// function onTextAreaInput(event) {
-//   localStorage.setItem(LOCAL_STORAGE_KEY, event.target.value);
-// }
-
-// function onFormSubmit(event) {
-//   event.preventDefault();
-//   event.target.reset();
-//   localStorage.removeItem(LOCAL_STORAGE_KEY);
-// }
-
 const LOCAL_STORAGE_KEY = 'feedback-form-state';
 
 const form = document.querySelector('.feedback-form');
